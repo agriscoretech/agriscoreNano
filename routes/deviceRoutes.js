6e96a5d97cc192c27425e5226b26a2d8da936d8d
mrsingh19 <piyushsingh0069@gmail.com>
2026-03-19 00:59:32 +0530

    backend ready

--- routes/deviceRoutes.js
+++ routes/deviceRoutes.js
@@ -3,8 +3,13 @@ import { getDevices, createDevice } from "../controllers/deviceController.js";
 import { verifyToken, verifyAdmin } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
+// Admin routes
 router.get("/", verifyToken, verifyAdmin, getDevices);
 router.post("/", verifyToken, verifyAdmin, createDevice);
+
+// Farmer routes (no admin check)
+router.get("/farmer", verifyToken, getDevices);
+router.post("/farmer", verifyToken, createDevice);
 
 
 export default router;
